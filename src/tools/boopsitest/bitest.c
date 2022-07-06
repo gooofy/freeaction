cc767158f8c46b90e3ccb3fbf51f237311c3f776
Guenter Bartsch <guenter@zamia.org>
2022-07-06 09:37:07 +0200

    strip out unneccessary code

--- src/tools/boopsitest/bitest.c
+++ src/tools/boopsitest/bitest.c
@@ -20,10 +20,8 @@
 #include <inline/utility.h>
 
 #define PROPGADGET_ID       1L
-#define INTGADGET_ID        2L
 #define PROPGADGETWIDTH     10L
 #define PROPGADGETHEIGHT    80L
-#define INTGADGETHEIGHT     18L
 #define VISIBLE             10L
 #define TOTAL               100L
 #define INITIALVAL          25L
@@ -31,33 +29,18 @@
 #define MINWINDOWHEIGHT     (PROPGADGETHEIGHT + 70)
 #define MAXCHARS            3L
 
-
 int __nocommandline=1; /* Disable commandline parsing  */
 int __initlibraries=0; /* Disable auto-library-opening */
 
 extern struct WBStartup *_WBenchMsg;
 
-//extern struct ExecBase      *SysBase;
 struct DosLibrary    *DOSBase       = NULL;
 struct IntuitionBase *IntuitionBase = NULL;
 struct UtilityBase   *UtilityBase   = NULL;
 
 struct Window        *w             = NULL;
 struct IntuiMessage  *msg;
-struct Gadget        *prop=NULL, *integer=NULL;
-
-/* The attribute mapping lists */
-struct TagItem prop2intmap[] =    /* This tells the prop gadget to */
-{                                 /* map its PGA_Top attribute to  */
-    {PGA_Top,   STRINGA_LongVal}, /* STRINGA_LongVal when it       */
-    {TAG_END,}                    /* issues an update about the    */
-};                                /* change to its PGA_Top value.  */
-
-struct TagItem int2propmap[] =    /* This tells the string gadget */
-{                                 /* to map its STRINGA_LongVal   */
-    {STRINGA_LongVal,   PGA_Top}, /* attribute to PGA_Top when it */
-    {TAG_END,}                    /* issues an update.            */
-};
+struct Gadget        *prop=NULL;
 
 static BPTR           astdout       = 0;
 
@@ -75,8 +58,6 @@ void cleanexit(char *msg)
 {
     if (msg)
         DPRINTF (msg);
-    if (integer)
-        DisposeObject(integer);
     if (prop)
         DisposeObject(prop);
     if (w)
@@ -86,14 +67,6 @@ void cleanexit(char *msg)
         CloseLibrary((struct Library*)DOSBase);
     if (IntuitionBase)
         CloseLibrary((struct Library*)IntuitionBase);
-#if 0
-    if (WindowBase)
-        CloseLibrary(WindowBase);
-    if (LayoutBase)
-        CloseLibrary(LayoutBase);
-    if (ButtonBase)
-        CloseLibrary(ButtonBase);
-#endif
 
     exit(0);
 }
@@ -125,70 +98,27 @@ int main(void)
     DPRINTF ("window opened.");
 
     prop = (struct Gadget *)NewObject(NULL, (STRPTR)"propgclass",
-                                      GA_ID,     PROPGADGET_ID,        /* These are defined by gadgetclass and         */
-                                      GA_Top,    (w->BorderTop) + 5L,  /* correspond to similarly named fields in      */
-                                      GA_Left,   (w->BorderLeft) + 5L, /* the Gadget structure.                        */
+                                      GA_ID,     PROPGADGET_ID,
+                                      GA_Top,    (w->BorderTop) + 5L,
+                                      GA_Left,   (w->BorderLeft) + 5L,
                                       GA_Width,  PROPGADGETWIDTH,
                                       GA_Height, PROPGADGETHEIGHT,
-
-                                      ICA_MAP,      (LONG) prop2intmap, /* The prop gadget's attribute map */
-
-                                      /* The rest of this gadget's attributes are defined by propgclass. */
-                                      PGA_Total,     TOTAL,          /* This is the integer range of the prop gadget.  */
-                                      PGA_Top,       INITIALVAL,     /* The initial integer value of the prop gadget.  */
-
-                                      PGA_Visible,   VISIBLE, /* This determines how much of the prop gadget area is   */
-                                                              /* covered by the prop gadget's knob, or how much of     */
-                                                              /* the gadget's TOTAL range is taken up by the prop      */
-                                                              /* gadget's knob.                                        */
-
-                                      PGA_NewLook,   TRUE,    /* Use new-look prop gadget imagery */
+                                      PGA_Total,     TOTAL,
+                                      PGA_Top,       INITIALVAL,
+                                      PGA_Visible,   VISIBLE,
+                                      PGA_NewLook,   TRUE,
                                       TAG_END);
     if (!prop)
         cleanexit ("failed to create proportional gadget object\n");
     DPRINTF ("proportional gadget object created.\n");
 
-    integer = (struct Gadget *)NewObject(NULL, (STRPTR) "strgclass",
-                                         GA_ID,      INTGADGET_ID,         /* Parameters for the Gadget structure     */
-                                         GA_Top,     (w->BorderTop) + 5L,
-                                         GA_Left,    (w->BorderLeft) + PROPGADGETWIDTH + 10L,
-                                         GA_Width,   MINWINDOWWIDTH -
-                                                       (w->BorderLeft + w->BorderRight +
-                                                       PROPGADGETWIDTH + 15L),
-                                         GA_Height,  INTGADGETHEIGHT,
-
-                                         ICA_MAP,    (LONG) int2propmap,           /* The attribute map */
-                                         ICA_TARGET, (LONG) prop,                  /* plus the target.  */
-
-                                                 /* Th GA_Previous attribute is defined by gadgetclass and is used to */
-                                                 /* wedge a new gadget into a list of gadget's linked by their        */
-                                                 /* Gadget.NextGadget field.  When NewObject() creates this gadget,   */
-                                                 /* it inserts the new gadget into this list behind the GA_Previous   */
-                                                 /* gadget. This attribute is a pointer to the previous gadget        */
-                                                 /* (struct Gadget *).  This attribute cannot be used to link new     */
-                                                 /* gadgetsinto the gadget list of an open window or requester,       */
-                                         GA_Previous, (LONG) prop,  /* use AddGList() instead.                               */
-
-                                         STRINGA_LongVal,  INITIALVAL, /* These attributes are defined by strgclass.  */
-                                         STRINGA_MaxChars, MAXCHARS,   /* The first contains the value of the         */
-                                         TAG_END);                     /* integer string gadget. The second is the    */
-                                                                       /* maximum number of characters the user is    */
-                                                                       /* allowed to type into the gadget.            */
-    if (!integer)
-        cleanexit ("failed to create integer gadget object\n");
-
-    SetGadgetAttrs(prop, w, NULL, /* Because the integer string gadget did not   */
-        ICA_TARGET, (LONG) integer,      /* exist when this example created the prop    */
-        TAG_END);                 /* gadget, it had to wait to set the           */
-                                  /* ICA_Target of the prop gadget.              */
-
-    AddGList(w, prop, -1, -1, NULL);  /* Add the gadgets to the                  */
-    RefreshGList(prop, w, NULL, -1);  /* window and display them.                */
+    AddGList(w, prop, -1, -1, NULL);
+    RefreshGList(prop, w, NULL, -1);
 
     BOOL done = FALSE;
 
-    while (done == FALSE)     /* Wait for the user to click */
-    {                         /* the window close gadget.   */
+    while (done == FALSE)
+    {
         WaitPort((struct MsgPort *)w->UserPort);
         while ( (msg = (struct IntuiMessage *) GetMsg((struct MsgPort *)w->UserPort)) )
         {
